45c527ad091e79371fee890cf547f94f936253b2
Andy Lamothe-Futu <2272637@collegemv.qc.ca>
2024-05-07 12:52:38 -0400

    Modification de linterface

--- jeuEchec/src/main/java/ca/mv/projet/models/pieces/Cavalier.java
+++ jeuEchec/src/main/java/ca/mv/projet/models/pieces/Cavalier.java
@@ -12,6 +12,21 @@ public class Cavalier extends Piece {
     @Override
     public boolean peutBouger(Position posCourante, Position posDestination, Echiquier echiquier) {
         // TODO: remplacer par le code approprié
+        int mouvementY = posDestination.getY() - posCourante.getY();
+        int mouvementX = posDestination.getX() - posCourante.getX();
+
+
+        if (
+                (mouvementX == 2 && mouvementY == 1) || (mouvementX == -2 && mouvementY == 1) ||
+                        (mouvementX == 2 && mouvementY == -1) || (mouvementX == -2 && mouvementY == -1) ||
+                        (mouvementX == 1 && mouvementY == 2) || (mouvementX == -1 && mouvementY == 2) ||
+                        (mouvementX == 1 && mouvementY == -2) || (mouvementX == -1 && mouvementY == -2)
+        )
+
+        {
+            if (echiquier.get);
+        }
+
         System.out.println("peutBouger cavalier");
         return true;
     }
